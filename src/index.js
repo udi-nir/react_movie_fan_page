c7c5d46cfdc9180b47bc8f8e54b3709468cb97b1
udi-nir <nir.udi@gmail.com>
2019-11-19 17:49:39 +0200

    pass resource to the movie item component.

--- src/index.js
+++ src/index.js
@@ -17,26 +17,31 @@ const App = () => (
     <MovieItem
       title="Ice Age"
       img="../../../../public/resources/movie1.jpg"
+      resource={movie1}
       img_="https://images-na.ssl-images-amazon.com/images/I/51A3PXFRV6L._SY445_.jpg"
     />
     <MovieItem
       title="Ice Age 2"
       img="../../../../public/resources/movie2.jpg"
+      resource={movie2}
       img_="https://images-na.ssl-images-amazon.com/images/I/51zPYE7eZTL._SY445_.jpg"
     />
     <MovieItem
       title="Ice Age 3"
       img="../../../../public/resources/movie3.jpg"
+      resource={movie3}
       img_="https://nammi.is/pub/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/i/c/ice_age_3_6.jpg"
     />
     <MovieItem
       title="Ice Age 4"
       img="../../../../public/resources/movie4.jpg"
+      resource={movie4}
       img_="https://images-na.ssl-images-amazon.com/images/I/71hLvTZg89L._SL1500_.jpg"
     />
     <MovieItem
       title="Ice Age 5"
       img="../../../../public/resources/movie5.jpg"
+      resource={movie5}
       img_="https://images-na.ssl-images-amazon.com/images/I/61lm2MHMS7L._SY445_.jpg"
     />
   </div>
--- src/views/movieItem/MovieItem.js
+++ src/views/movieItem/MovieItem.js
@@ -3,12 +3,12 @@ import "./style.css";
 //import { render } from "react-dom";
 
 function MovieItem(props) {
-  var src = require(props.img);
+  //var src = require(props.img);
   //var src = require("../../../../public/resources/movie5.jpg");
 
   return (
     <div className="movieBlock">
-      <img src={src} alt={props.title} className="movieImg" />
+      <img src={props.resource} alt={props.title} className="movieImg" />
       <div className="movieText">{props.title}</div>
     </div>
   );
