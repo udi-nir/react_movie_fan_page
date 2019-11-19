9bc161501caf749e61ff4aa874d634261208103e
udi-nir <nir.udi@gmail.com>
2019-11-19 16:23:48 +0200

    use local resources

--- src/index.js
+++ src/index.js
@@ -4,28 +4,40 @@ import { render } from "react-dom";
 import Header from "./views/header/Header";
 import MovieItem from "./views/movieItem/MovieItem";
 
+// get resources, so they are available when we need them:
+import movie1 from "../public/resources/movie1.jpg";
+import movie2 from "../public/resources/movie2.jpg";
+import movie3 from "../public/resources/movie3.jpg";
+import movie4 from "../public/resources/movie4.jpg";
+import movie5 from "../public/resources/movie5.jpg";
+
 const App = () => (
   <div>
     <Header />
     <MovieItem
       title="Ice Age"
-      img="https://images-na.ssl-images-amazon.com/images/I/51A3PXFRV6L._SY445_.jpg"
+      img="../../../../public/resources/movie1.jpg"
+      img_="https://images-na.ssl-images-amazon.com/images/I/51A3PXFRV6L._SY445_.jpg"
     />
     <MovieItem
       title="Ice Age 2"
-      img="https://images-na.ssl-images-amazon.com/images/I/51zPYE7eZTL._SY445_.jpg"
+      img="../../../../public/resources/movie2.jpg"
+      img_="https://images-na.ssl-images-amazon.com/images/I/51zPYE7eZTL._SY445_.jpg"
     />
     <MovieItem
       title="Ice Age 3"
-      img="https://nammi.is/pub/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/i/c/ice_age_3_6.jpg"
+      img="../../../../public/resources/movie3.jpg"
+      img_="https://nammi.is/pub/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/i/c/ice_age_3_6.jpg"
     />
     <MovieItem
       title="Ice Age 4"
-      img="https://images-na.ssl-images-amazon.com/images/I/71hLvTZg89L._SL1500_.jpg"
+      img="../../../../public/resources/movie4.jpg"
+      img_="https://images-na.ssl-images-amazon.com/images/I/71hLvTZg89L._SL1500_.jpg"
     />
     <MovieItem
       title="Ice Age 5"
-      img="https://images-na.ssl-images-amazon.com/images/I/61lm2MHMS7L._SY445_.jpg"
+      img="../../../../public/resources/movie5.jpg"
+      img_="https://images-na.ssl-images-amazon.com/images/I/61lm2MHMS7L._SY445_.jpg"
     />
   </div>
 );
--- src/views/movieItem/MovieItem.js
+++ src/views/movieItem/MovieItem.js
@@ -3,12 +3,17 @@ import "./style.css";
 //import { render } from "react-dom";
 
 function MovieItem(props) {
+  var src = require(props.img);
+  //var src = require("../../../../public/resources/movie5.jpg");
+
   return (
     <div className="movieBlock">
-      <img src={props.img} alt={props.title} className="movieImg" />
+      <img src={src} alt={props.title} className="movieImg" />
       <div className="movieText">{props.title}</div>
     </div>
   );
 }
+
+// <img src={props.img} alt={props.title} className="movieImg" />
 
 export default MovieItem;
