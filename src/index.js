52ce6286a4e5fa34af88b65935926fd79a3d0391
udi-nir <nir.udi@gmail.com>
2019-11-20 07:14:28 +0200

    Change movies titles / width.

--- src/index.js
+++ src/index.js
@@ -21,25 +21,25 @@ const App = () => (
       img_="https://images-na.ssl-images-amazon.com/images/I/51A3PXFRV6L._SY445_.jpg"
     />
     <MovieItem
-      title="Ice Age 2"
+      title="The Meltdown"
       img="../../../../public/resources/movie2.jpg"
       resource={movie2}
       img_="https://images-na.ssl-images-amazon.com/images/I/51zPYE7eZTL._SY445_.jpg"
     />
     <MovieItem
-      title="Ice Age 3"
+      title="Dawn of the Dinosaurs"
       img="../../../../public/resources/movie3.jpg"
       resource={movie3}
       img_="https://nammi.is/pub/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/i/c/ice_age_3_6.jpg"
     />
     <MovieItem
-      title="Ice Age 4"
+      title="Continental Drift"
       img="../../../../public/resources/movie4.jpg"
       resource={movie4}
       img_="https://images-na.ssl-images-amazon.com/images/I/71hLvTZg89L._SL1500_.jpg"
     />
     <MovieItem
-      title="Ice Age 5"
+      title="Collision Course"
       img="../../../../public/resources/movie5.jpg"
       resource={movie5}
       img_="https://images-na.ssl-images-amazon.com/images/I/61lm2MHMS7L._SY445_.jpg"
